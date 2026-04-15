57abae9dd0a4c1b88e06f9f23c380dd3894b97b8
BM-Haoh <joao.massabki@gmail.com>
2026-04-15 15:27:30 -0300

    concertando alguns erros tentativa 2

--- ai-python/app/app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""SQLAlchemy models."""
-# Add models here
-# Example:
-# from app.core.database import Base
-# from sqlalchemy import Column, Integer, String
-#
-# class User(Base):
-#     __tablename__ = "users"
-#     id = Column(Integer, primary_key=True, index=True)
